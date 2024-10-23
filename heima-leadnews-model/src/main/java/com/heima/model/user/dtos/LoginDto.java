f84395e847f7a1dd13adda885ee783f0d2c2799b
zhangchenghao <2282610045@qq.com>
2024-10-23 14:08:45 +0800

    knife4j

--- heima-leadnews-model/src/main/java/com/heima/model/user/dtos/LoginDto.java
+++ heima-leadnews-model/src/main/java/com/heima/model/user/dtos/LoginDto.java
@@ -1,4 +1,5 @@
 package com.heima.model.user.dtos;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
@@ -6,11 +7,11 @@ public class LoginDto {
     /**
      * 手机号字段，用于存储用户手机号码
      */
+    @ApiModelProperty(value = "手机号",required = true)
     private String phone;
-
-
     /**
      * 密码字段，用于存储用户账户密码
      */
+    @ApiModelProperty(value = "密码",required = true)
     private String password;
 }
--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/service/impl/ApUserServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/service/impl/ApUserServiceImpl.java
@@ -10,58 +10,63 @@ import com.heima.user.mapper.ApUserMapper;
 import com.heima.user.service.ApUserService;
 import com.heima.utils.common.AppJwtUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
-import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+
 
 @Service
 @Transactional
 @Slf4j
 public class ApUserServiceImpl extends ServiceImpl<ApUserMapper, ApUser> implements ApUserService {
     /**
+     * app端登录功能
+     * @param dto
+     * @return
+     */
+    /**
      * 用户登录方法
+     * 支持正常登录和游客登录两种方式
+     * 正常登录需要提供手机号和密码，游客登录则不需要任何参数
      *
-     * @param dto 登录信息，包含用户手机号和密码
-     * @return 登录结果，包括用户信息和token
+     * @param dto 包含登录信息的DTO，包括手机号和密码
+     * @return 返回登录结果，包括用户信息和JWT令牌
      */
     @Override
     public ResponseResult login(LoginDto dto) {
-        // 检查用户是否输入了手机号和密码
-        if (StringUtils.isEmpty(dto.getPhone()) && StringUtils.isEmpty(dto.getPassword())){
-            // 根据手机号查询数据库中的用户信息
+        //1.正常登录 用户名和密码
+        if(StringUtils.isNotBlank(dto.getPhone()) && StringUtils.isNotBlank(dto.getPassword())){
+            //1.1 根据手机号查询用户信息
             ApUser dbUser = getOne(Wrappers.<ApUser>lambdaQuery().eq(ApUser::getPhone, dto.getPhone()));
-            // 如果用户不存在，返回错误信息
             if(dbUser == null){
-                return ResponseResult.errorResult(AppHttpCodeEnum.DATA_NOT_EXIST, "User not found");
+                return ResponseResult.errorResult(AppHttpCodeEnum.DATA_NOT_EXIST,"用户信息不存在");
             }
-            // 获取用户密码盐值
+
+            //1.2 比对密码
             String salt = dbUser.getSalt();
-            // 获取用户输入的密码
             String password = dto.getPassword();
-            // 将用户输入的密码和盐值一起进行MD5加密
             String pswd = DigestUtils.md5DigestAsHex((password + salt).getBytes());
-            // 比较加密后的密码和数据库中的密码是否一致
-            if (!pswd.equals(dbUser.getPassword())){
-                return ResponseResult.errorResult(AppHttpCodeEnum.LOGIN_PASSWORD_ERROR, "Password error");
+            if(!pswd.equals(dbUser.getPassword())){
+                return ResponseResult.errorResult(AppHttpCodeEnum.LOGIN_PASSWORD_ERROR);
             }
-            // 生成用户登录token
+
+            //1.3 返回数据  jwt  user
             String token = AppJwtUtil.getToken(dbUser.getId().longValue());
-            // 创建返回的用户信息map
-            Map<String, Object> map = new HashMap<>();
-            map.put("token", token);
-            // 清除用户信息中的盐值和密码
+            Map<String,Object> map = new HashMap<>();
+            map.put("token",token);
             dbUser.setSalt("");
             dbUser.setPassword("");
-            map.put("user", dbUser);
+            map.put("user",dbUser);
+
             return ResponseResult.okResult(map);
-        }else{
-            // 如果用户未输入手机号或密码，返回默认登录结果
-            Map<String, Object> map = new HashMap<>();
-            map.put("token", AppJwtUtil.getToken(0L));
+        }else {
+            //2.游客登录
+            Map<String,Object> map = new HashMap<>();
+            map.put("token",AppJwtUtil.getToken(0L));
             return ResponseResult.okResult(map);
         }
     }
